ad27bd1930e38b4a1aab43227530397aaf40fd45
cwilkins-8 <71436174+cwilkins-8@users.noreply.github.com>
2020-11-30 22:52:22 +0000

    Add files via upload

---
+++ 4.3.py
@@ -0,0 +1,32 @@
+#4.3 Counting to Twenty
+
+#for value in range(1,100000):
+    #print(value)
+
+#numbers = list(range(1,100001))
+
+#print(min(numbers))
+#print(max(numbers))
+#print(sum(numbers))
+
+
+#4.6 Odd numbers
+
+#new = list(range(1,21,2))
+#print(new)
+
+#4.7
+
+#three = list(range(3,31,3))
+#for number in three:
+   # print(number)
+
+#4.8
+
+cubes = []
+for x in range(1,11):
+    cube =x**3
+    cubes.append(cube)
+
+for cube in cubes:
+    print(cube)
